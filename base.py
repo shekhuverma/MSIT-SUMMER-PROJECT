ec158696ef545dcec7ddd7ab366fd90e4d6f2baf
shekhuverma <shekharvrm47@gmail.com>
2017-06-23 21:21:25 +0530

    initial commit

--- base.py
+++ base.py
@@ -1,29 +1,45 @@
 import cv2
+import pyautogui
 import numpy as np
-fgbg = cv2.createBackgroundSubtractorMOG2()
 cam=cv2.VideoCapture(0)
 while True:
     ret,img=cam.read()
-    img=fgbg.apply(img)
-##    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-##    gray = cv2.GaussianBlur(gray, (11, 11), 0)
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (11, 11), 0)
     ret,img2=cam.read()
-    img2=fgbg.apply(img2)
-##    gray2=cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-##    gray2 = cv2.GaussianBlur(gray2, (11, 11), 0)
+    gray2=cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.GaussianBlur(gray2, (11, 11), 0)
     #detecting motion by subtracitng frames
-    frameDelta = cv2.absdiff(img2, img)
+    frameDelta = cv2.absdiff(gray2,gray)
     thresh = cv2.threshold(frameDelta, 30, 255, cv2.THRESH_BINARY)[1]
-##    thresh = cv2.dilate(thresh, None, iterations=3)
+    thresh = cv2.dilate(thresh, None, iterations=3)
     #finding contours in the b/w image
-    img,cnts,_ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    for c in cnts:
-        if cv2.contourArea(c) < 100:
-            continue
-        (x,y, w, h) = cv2.boundingRect(c)
-# compute the bounding box for the contour, draw it on the frame,
-        cv2.rectangle(thresh, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.imshow("frameDelta",frameDelta)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    # only proceed if the radius meets a minimum size
+            if radius > 10:
+                    # draw the circle and centroid on the frame,
+                    # then update the list of tracked points
+                    cv2.circle(thresh, (int(x), int(y)), int(radius),
+                            (0, 255, 255), 2)
+                    cv2.circle(thresh, center, 5, (0, 0, 255), -1)
+    #width = 640 height 480
+    if center==None:
+        continue
+    else:
+        x=(center[0]*1920)/640
+        y=(center[1]*1080)/480
+        pyautogui.moveTo(x,y)
+##    cv2.imshow("frameDelta",frameDelta)
     cv2.imshow("thresh",thresh)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
