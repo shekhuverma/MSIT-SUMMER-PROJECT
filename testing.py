f32404368afaa7e01d2f9d28f6fadb04df956d07
shekhuverma <shekharvrm47@gmail.com>
2017-06-29 11:04:08 +0530

    Testing=> SVM AND HOG

--- testing.py
+++ testing.py
@@ -1,20 +1,43 @@
-import cv2,matplotlib
+import cv2,matplotlib,os
 import numpy as np
-import pyautogui
-import functions as f
-HIST=[]
-HOG=[]
-np.array(HIST)
-np.array(HOG)
-img=cv2.imread("test.jpg")
-print img.shape[0]
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-for a in range(0,len(img),8):#column
-    for b in range(0,len(img),8):#row
-        temp=img[b:b+8,a:a+8]
-        np.array(temp)
-        hist=cv2.calcHist([temp],[0],None,[256],[0,256])
-        print hist
-        break
-        HIST.append(temp)
+from PIL import Image
+path="hand_data"
+Descriptor=[]
+svm = cv2.ml.SVM_create()
+# Set SVM type
+svm.setType(cv2.ml.SVM_C_SVC)
+# Set SVM Kernel to Radial Basis Function (RBF) 
+svm.setKernel(cv2.ml.SVM_RBF)
+# Set parameter C
+svm.setC(100)
+# Set parameter Gamma
+svm.setGamma(20)
+#######descriptor values
+winSize = (20,20)
+blockSize = (10,10)
+blockStride = (5,5)
+cellSize = (10,10)
+nbins = 9
+derivAperture = 1
+winSigma = -1.
+histogramNormType = 0
+L2HysThreshold = 0.2
+gammaCorrection = 1
+nlevels = 64
+signedGradients = True
+hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,gammaCorrection,nlevels,signedGradients)
+## getting images from dataset
+paths=[os.path.join(path,x) for x in os.listdir(path)]
+for x in paths:
+    if x=="hand_data\Thumbs.db":
+        continue
+    try:
+        img=Image.open(x).convert('L')
+        img=np.array(img,dtype='uint8')
+        descriptor=hog.compute(img)
+        svm.train(Descriptor, cv2.ml.ROW_SAMPLE,[1])
+    except IOError:
+        continue
+# Save trained model 
+svm.save("digits_svm_model.yml");
 cv2.destroyAllWindows()
