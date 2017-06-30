4218b7e033896b74f977c409268b761ea712aae5
shekhuverma <shekharvrm47@gmail.com>
2017-06-30 12:52:30 +0530

    modified test.py

---
+++ features_testing.py
@@ -0,0 +1,4 @@
+from sklearn.externals import joblib
+while True:
+    temp=joblib.load("test.dat")
+    print temp
--- testing.py
+++ testing.py
@@ -1,35 +1,46 @@
 import cv2,matplotlib,os
 import numpy as np
 from PIL import Image
+from skimage.feature import hog
 from sklearn.svm import SVC
-path="hand_data"
+from sklearn.externals import joblib   
+pos_path="hand_data"
+neg_path="neg_data"
+Features=[]
+Features=np.array(Features)
+Labels=[]
 classifier=SVC(kernel='rbf',random_state=0)
-#######descriptor values
-winSize = (100,100)
-blockSize = (25,25)
-blockStride = (5,5)
-cellSize = (5,5)
-nbins = 9
-derivAperture = 1
-winSigma = -1.
-histogramNormType = 0
-L2HysThreshold = 0.2
-gammaCorrection = 1
-nlevels = 64
-signedGradients = True
-hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,gammaCorrection,nlevels, signedGradients)
-## getting images from dataset
-paths=[os.path.join(path,x) for x in os.listdir(path)]
-for x in paths:
+## getting positive images from dataset
+pos_paths=[os.path.join(pos_path,x) for x in os.listdir(pos_path)]
+for x in pos_paths:
     if x=="hand_data\Thumbs.db":
         continue
-    try:
+    try:    
         img=Image.open(x).convert('L')
         img=np.array(img)
-        descriptor=hog.compute(img)
-        classifier.fit(descriptor,[1])
+        fd, hog_image = hog(img, orientations=8, pixels_per_cell=(16, 16),
+                    cells_per_block=(1, 1), visualise=True)
+        joblib.dump(fd,"features.dat")
+        Features=np.append(Features,fd)
+        Labels.append(1)
     except IOError:
         continue
+##Getting negative images from dataset
+neg_paths=[os.path.join(neg_path,x) for x in os.listdir(neg_path)]
+for x in neg_paths:
+    if x=="hand_data\Thumbs.db":
+        continue
+    try:    
+        img=Image.open(x).convert('L')
+        img=np.array(img)
+        fd, hog_image = hog(img, orientations=8, pixels_per_cell=(16, 16),
+                    cells_per_block=(1, 1), visualise=True)
+        joblib.dump(fd,"features.dat")
+        Features=np.append(Features,fd)
+        Labels.append(0)
+    except IOError:
+        continue
+classifier.fit(Features,Labels)
 # Save trained model 
 svm.save("digits_svm_model.yml");
 cv2.destroyAllWindows()
