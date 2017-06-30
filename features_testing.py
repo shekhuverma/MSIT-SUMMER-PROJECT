10fc86a476914016abff3bd38d9594d5439d0327
shekhuverma <shekharvrm47@gmail.com>
2017-06-30 13:45:17 +0530

    test.py changes

--- features_testing.py
+++
@@ -1,4 +0,0 @@
-from sklearn.externals import joblib
-while True:
-    temp=joblib.load("test.dat")
-    print temp
--- testing.py
+++ testing.py
@@ -2,14 +2,13 @@ import cv2,matplotlib,os
 import numpy as np
 from PIL import Image
 from skimage.feature import hog
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.externals import joblib   
 pos_path="hand_data"
 neg_path="neg_data"
 Features=[]
-Features=np.array(Features)
 Labels=[]
-classifier=SVC(kernel='rbf',random_state=0)
+classifier=LinearSVC()
 ## getting positive images from dataset
 pos_paths=[os.path.join(pos_path,x) for x in os.listdir(pos_path)]
 for x in pos_paths:
@@ -20,8 +19,7 @@ for x in pos_paths:
         img=np.array(img)
         fd, hog_image = hog(img, orientations=8, pixels_per_cell=(16, 16),
                     cells_per_block=(1, 1), visualise=True)
-        joblib.dump(fd,"features.dat")
-        Features=np.append(Features,fd)
+        Features.append(fd)
         Labels.append(1)
     except IOError:
         continue
@@ -35,12 +33,13 @@ for x in neg_paths:
         img=np.array(img)
         fd, hog_image = hog(img, orientations=8, pixels_per_cell=(16, 16),
                     cells_per_block=(1, 1), visualise=True)
-        joblib.dump(fd,"features.dat")
-        Features=np.append(Features,fd)
+        Features.append(fd)
         Labels.append(0)
     except IOError:
         continue
-classifier.fit(Features,Labels)
+joblib.dump(Features,"features.pkl")
+Features=joblib.load("features.pkl")
+classifier.fit(features2,Labels)
 # Save trained model 
 svm.save("digits_svm_model.yml");
 cv2.destroyAllWindows()
