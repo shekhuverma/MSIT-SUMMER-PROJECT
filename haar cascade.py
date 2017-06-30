e52fd53082dbf2e21eb1922d5d3ef084622bdd80
shekhuverma <shekharvrm47@gmail.com>
2017-06-30 20:54:31 +0530

    organising

--- haar cascade.py
+++ haar cascade.py
@@ -2,7 +2,8 @@ import cv2,time,matplotlib
 import numpy as np
 import pyautogui
 import functions as f
-aGest=cv2.CascadeClassifier('aGest.xml')
+aGest=cv2.CascadeClassifier('haar_cascades\\aGest.xml')
+print aGest
 cam=cv2.VideoCapture(0)
 last_time=time.time()
 print "Open is red close is blue"
@@ -12,7 +13,6 @@ while True:
 ##    Open = open_palm.detectMultiScale(gray, 1.05, 6,minSize=(30,30),maxSize=(50,50))
 ##    Close = close_palm.detectMultiScale(gray, 1.3, 1,)
     Agest=aGest.detectMultiScale(gray, 1.3,1)
-    Agest=non_max_suppression_fast(Agest, 3)
     for (x,y,w,h) in Agest:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
 ##        x=x+w/2
