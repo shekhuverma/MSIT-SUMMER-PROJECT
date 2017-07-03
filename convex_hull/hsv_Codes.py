709d1363a0bc335b800a7b69272a15c6c9f42acd
shekhuverma <shekharvrm47@gmail.com>
2017-07-04 00:14:17 +0530

    Improvements

--- convex_hull/hsv_Codes.py
+++ convex_hull/hsv_Codes.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 import pickle
-print "Change the slider so that your hand will be coloured black (Choose the bes tpossible combination)"
+print "Adjust the sliders so that your hand will be become black (Choose the best possible combination)"
 cap = cv2.VideoCapture(0)
 # Creating a window for later use
 cv2.namedWindow('result')
-def nothing():
+def nothing(x):
     pass
 # Starting with 100's to prevent error while masking
 h,s,v = 100,100,100
@@ -14,6 +14,7 @@ h,s,v = 100,100,100
 cv2.createTrackbar('h', 'result',0,179,nothing)
 cv2.createTrackbar('s', 'result',0,255,nothing)
 cv2.createTrackbar('v', 'result',0,255,nothing)
+cv2.createTrackbar('threshold', 'result',0,255,nothing)
 while(1):
     _, frame = cap.read()
     #converting to HSV
@@ -22,6 +23,7 @@ while(1):
     h = cv2.getTrackbarPos('h','result')
     s = cv2.getTrackbarPos('s','result')
     v = cv2.getTrackbarPos('v','result')
+    th = cv2.getTrackbarPos('threshold','result')
     arr=[h,s,v]
     arr=np.array(arr,dtype=np.uint8)
     np.savetxt("config.txt",arr,delimiter="'")
@@ -32,9 +34,9 @@ while(1):
     mask = cv2.inRange(hsv,lower_blue, upper_blue)
 
     result = cv2.bitwise_and(frame,frame,mask = mask)
-
+    ret,thresh = cv2.threshold(result,th,255,cv2.THRESH_BINARY)
     cv2.imshow('result',result)
-
+    cv2.imshow('thresh',thresh)
     if cv2.waitKey(10) & 0xFF==ord("q"):
         break
 
